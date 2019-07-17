0eb2f510d1bf39b38c5e469e7b1cd12534bd9a40
algoORgoal <qufstar46@gmail.com>
2019-07-17 09:58:04 +0900

    rename files and initialize

--- app.js
+++ app.js
@@ -17,20 +17,7 @@ router.get('/bootstrap01_byeolchan', function (req, res) {
 res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/bootstrap01.html'));
 //__dirname : It will resolve to your project folder.
 });
-
-router.get('/bootstrap02_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/bootstrap02.html'));
-//__dirname : It will resolve to your project folder.
-});
-
-//static links for Junguk
-//Your code goes here...
-router.get('/colors-images', function (req, res) {
-res.sendFile(path.join(__dirname + '../views/bootstrap_junguk/colors-images.html'));
-//__dirname : It will resolve to your project folder.
-});
     
-
 app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
