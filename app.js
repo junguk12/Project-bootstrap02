14ba3ee5d3095bc9269660603d29b62eb7ff6aed
algoORgoal <qufstar46@gmail.com>
2019-07-18 09:39:37 +0900

    create dropdowns

--- app.js
+++ app.js
@@ -8,51 +8,65 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-res.sendFile(path.join(__dirname + '/index.html'));
-//__dirname : It will resolve to your project folder.
+    res.sendFile(path.join(__dirname + '/index.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
 //static links for Byeolchan
-router.get('/bootstrap01_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/typography01.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/buttons01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/buttons01.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap02_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/typography02.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/buttons02_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/buttons02.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap03_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/buttons01.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/carousel01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/carousel01.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap04_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/buttons02.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/collapse01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/collapse01.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap05_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/collapse01.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/dropdowns_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/dropdowns.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap06_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/image01.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/image01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/image01.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap07_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/table01.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/table01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/table01.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-router.get('/bootstrap07_byeolchan', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/table02.html'));
-//__dirname : It will resolve to your project folder.
+router.get('/table02_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/table02.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
+router.get('/try_main_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/try_main.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('//typography01_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/typography01.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/typography02_byeolchan', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/bootstrap_byeolchan/typography02.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
 app.use('/', router);
 
